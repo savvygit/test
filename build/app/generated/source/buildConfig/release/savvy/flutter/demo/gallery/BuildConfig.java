a99e4af583210a6bcb3dde65c97e63504d6f7758
savvy2 <savvy2@savvy2s-Mac-mini.local>
2018-08-27 20:44:08 +0200

    ini1

---
+++ build/app/generated/source/buildConfig/release/savvy/flutter/demo/gallery/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package savvy.flutter.demo.gallery;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "savvy.flutter.demo.gallery";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 28000000;
+  public static final String VERSION_NAME = "0.7.0";
+}
